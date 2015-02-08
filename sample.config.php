22ae9304fd091166e815a3e0077d14bdb9fbf2ce
vakata <git@vakata.com>
2015-02-09 00:10:55 +0200

    changed config

--- sample.config.php
+++ sample.config.php
@@ -6,30 +6,31 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . '
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 \raichu\Raichu::config([
-	'appname'          => 'demoapp',
-	'database'         => 'mysqli://root@127.0.0.1/raichu',
-	'memcache'         => false,
-	'filecache'        => __DIR__,
-	'sessionDatabase'  => true,
-	'sessionCache'     => false,
-	'session'          => true,
-	'csrf'             => true,
-	'csp'              => false,
-	'uploadsDirectory' => __DIR__,
-	'uploadsDatabase'  => true,
-	'user'             => [
-		'oauth'           => [
-			'facebook'        => [ ],
-			'google'          => [ ],
-			'microsoft'       => [ ],
-			'github'          => [ ],
-			'linkedin'        => [ ]
+	'database' => 'mysqli://root@127.0.0.1/raichu',
+	'cache'    => 'file://' . __DIR__, // memcache://127.0.0.1
+	'session'  => [
+		'storage'    => 'database', // or 'cache' or 'file'
+		'location'   => 'sessions' // table or path or null
+	],
+	'uploads'  => [
+		'directory'  => __DIR__,
+		'database'   => 'uploads' // table or null
+	],
+	'user'     => [
+		'oauth'      => [
+			'facebook'  => [ ],
+			'google'    => [ ],
+			'microsoft' => [ ],
+			'github'    => [ ],
+			'linkedin'  => [ ]
 		],
-		//'ldap'            => 'is-bg.net',
-		'token'           => '*S3cr$T(t0%En_KeY)*',
-		'password'        => true,
-		'certificate'     => true,
-		'session'         => true,
-		'database'        => true
-	]
+		'ldap'        => false, // domain here
+		'token'       => 'secret_token_key_here',
+		'password'    => 'users_password',
+		'certificate' => true,
+		'session'     => true,
+		'database'    => 'users'
+	],
+	'csp'      => false,
+	'csrf'     => true
 ]);
