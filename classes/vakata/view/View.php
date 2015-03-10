91dd6a53fd52aca69769b63f05b6d72866ad172e
vakata <git@vakata.com>
2015-03-10 15:58:24 +0200

    added static dir to view class as well as normalization

--- classes/vakata/view/View.php
+++ classes/vakata/view/View.php
@@ -3,21 +3,39 @@ namespace vakata\view;
 
 class View
 {
+	protected static $vdir = '';
 	protected $file = null;
 	protected $data = [];
 
 	public function __construct($file, array $data = []) {
-		$this->file = $file;
+		$this->file = $this->normalize($file);
 		$this->data = $data;
 	}
+	protected function normalize($view) {
+		if(static::$vdir) {
+			$view = static::$vdir . DIRECTORY_SEPARATOR . $view;
+		}
+		if(!preg_match('(\.php$)i',$view)) {
+			$view .= '.php';
+		}
+		if(!is_file($view) || !is_readable($view)) {
+			throw new ViewException('View not found', 404);
+		}
+		return $view;
+	}
+
 	public function add($name, $value) {
 		$this->data[$name] = $value;
+		return $this;
 	}
 	public function render() {
 		extract($this->data);
 		ob_start();
 		include $this->file;
 		return ob_get_clean();
+	}
+	public static function dir($dir) {
+		static::$vdir = realpath($dir);
 	}
 	public static function get($file, $data = null) {
 		return (new self($file, $data))->render();
---
+++ classes/vakata/view/ViewException.php
@@ -0,0 +1,6 @@
+<?php
+namespace vakata\view;
+
+class ViewException extends \Exception
+{
+}
