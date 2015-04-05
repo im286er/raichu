a7990a97361f6cd76a9ee048a671e51bd6fee5eb
vakata <git@vakata.com>
2015-04-05 22:46:19 +0300

    removed extra files

--- src/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-spl_autoload_register(function ($class) {
-	$class = __DIR__ . DIRECTORY_SEPARATOR . trim(str_replace('\\', DIRECTORY_SEPARATOR, $class), DIRECTORY_SEPARATOR) . '.php';
-	if(is_file($class) && is_readable($class)) {
-		require_once $class;
-	}
-});
