b39995be1797836a9cdcfac97067cdc0b9d13473
vakata <git@vakata.com>
2015-05-08 19:46:25 +0300

    changes to localization

--- src/vakata/localization/Lang.php
+++ src/vakata/localization/Lang.php
@@ -4,12 +4,29 @@ namespace vakata\localization;
 class Lang
 {
 	protected $data = [];
+	protected $lang = [];
 	protected $dflt = null;
 
-	public function add($code, $data, $dflt = false) {
-		$this->data[$code] = $data;
-		if($dflt) {
-			$this->setDefault($code);
+	public function add($code, $data) {
+		if(!isset($this->data[$code])) {
+			$this->data[$code] = [];
+		}
+		if(is_string($data)) {
+			$this->data[$code][] = $data;
+			if(isset($this->lang[$code])) {
+				if(is_file($data)) {
+					$temp = @json_decode(file_get_contents($data), true);
+					if($temp) {
+						$this->lang[$code] = array_merge($temp, $this->lang[$code]);
+					}
+				}
+			}
+		}
+		if(is_array($data)) {
+			if(!isset($this->lang[$code])) {
+				$this->load($code);
+			}
+			$this->lang[$code] = array_merge($data, $this->lang[$code]);
 		}
 		return $this;
 	}
@@ -45,16 +62,59 @@ class Lang
 		}
 		return $key;
 	}
-
-	protected function parse($code, $key, $count = 0, array $replace = []) {
-		if(is_string($this->data[$code])) {
-			$this->data[$code] = @json_decode(file_get_contents($this->data[$code]), true);
+	public function __invoke($key, $count = 0, array $replace = [], $code = null) {
+		return $this->get($key, $count, $replace, $code);
+	}
+	public function dump($code = null) {
+		if($code === null) {
+			$code = isset($this->dflt) && isset($this->data[$this->dflt]) ? $this->dflt : key($this->data);
 		}
+		if(!isset($this->lang[$code])) {
+			$this->load($code);
+		}
+		$result = [];
+		$this->traverse($this->lang[$code], $result, $code);
+		return $result;
+	}
+
+	protected function traverse($value, &$result, $key = '') {
+		foreach($value as $k => $v) {
+			if(is_string($v)) {
+				$result[$key . '[' . $k . ']'] = $v;
+			}
+			if(is_array($v)) {
+				$this->traverse($v, $result, $key . '[' . $k . ']');
+			}
+		}
+	}
+	protected function load($code) {
 		if(!isset($this->data[$code])) {
 			return null;
 		}
+		if(!isset($this->lang[$code])) {
+			$this->lang[$code] = [];
+		}
+		foreach($this->data[$code] as $file) {
+			if(is_file($file)) {
+				$temp = @json_decode(file_get_contents($file), true);
+				if($temp) {
+					$this->lang[$code] = array_merge($temp, $this->lang[$code]);
+				}
+			}
+		}
+	}
+	protected function parse($code, $key, $count = 0, array $replace = []) {
+		if(!isset($this->data[$code])) {
+			return null;
+		}
+		if(!isset($this->lang[$code])) {
+			$this->load($code);
+		}
+		if(!isset($this->lang[$code])) {
+			return null;
+		}
 		$tmp = explode('.', $key);
-		$val = $this->data[$code];
+		$val = $this->lang[$code];
 		foreach($tmp as $k) {
 			if(!isset($val[$k])) {
 				return null;
