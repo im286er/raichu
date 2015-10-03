07417bb34f10afc167cf9194666d51e390d7edfc
vakata <git@vakata.com>
2015-10-04 00:21:24 +0300

    added Config class

---
+++ src/vakata/config/Config.php
@@ -0,0 +1,35 @@
+<?php
+namespace vakata\config;
+
+class Config
+{
+	protected $config = [];
+
+	public function __construct(array $config = []) {
+		$this->config = $config;
+	}
+
+	public function get($key, $default = null) {
+		$key = explode('.', $key);
+		$tmp = $this->config;
+		foreach ($key as $k) {
+			if (!isset($tmp[$k])) {
+				return $default;
+			}
+			$tmp = $tmp[$k];
+		}
+		return $tmp;
+	}
+
+	public  function set($key, $value = null) {
+		$key = explode('.', $key);
+		$tmp = &$this->config;
+		foreach ($key as $k) {
+			if (!isset($tmp[$k])) {
+				$tmp[$k] = [];
+			}
+			$tmp = &$tmp[$k];
+		}
+		return $tmp = is_array($tmp) && is_array($value) && count($tmp) ? array_merge($tmp, $value) : $value;
+	}
+}
